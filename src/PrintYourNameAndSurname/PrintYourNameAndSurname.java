1c664547b8590780b108ff99c52958abb6ac2e00
Salvatore <salvodanna@yahoo.it>
2023-06-07 18:02:39 +0200

    first commit

---
+++ src/PrintYourNameAndSurname/PrintYourNameAndSurname.java
@@ -0,0 +1,9 @@
+package PrintYourNameAndSurname;
+
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        System.out.print("Salvatore ");
+        System.out.print("D'anna");
+    }
+
+}
